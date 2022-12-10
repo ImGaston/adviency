5a75a72ffcd624dd3acf4533c350ee19de5d59eb
Gaston <corbalan.gaston@outlook.com>
2022-12-10 12:10:57 -0300

    setup dia-3

---
+++ dia-3/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+// import reactLogo from './assets/react.svg';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Hola</h1>
+    </div>
+  );
+}
+
+export default App;
